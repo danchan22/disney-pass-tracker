0e36f58f873680cbb73f8c2180a21fcddef61d3d
danchan22 <daniel.chancellor@gmail.com>
2026-09-14 16:58:47 -0400

    Update landMappings.ts

--- components/Shared/LiveWaitTimes/landMappings.ts
+++ components/Shared/LiveWaitTimes/landMappings.ts
@@ -1,7 +1,12 @@
 import { PARK_ATTRACTIONS_BY_LAND } from '../../../lib/constants';
 
 const cleanStr = (s: string) =>
-  (s || '').toLowerCase().replace(/&/g, 'and').replace(/[’'"]/g, '').replace(/[^a-z0-9]/g, '');
+  (s || '')
+    .toLowerCase()
+    .replace(/&amp;/g, 'and')
+    .replace(/&/g, 'and')
+    .replace(/[’'"]/g, '')
+    .replace(/[^a-z0-9]/g, '');
 
 export const getLandForRide = (parkName: string, rideName: string): string => {
   const parkLands = PARK_ATTRACTIONS_BY_LAND[parkName];
